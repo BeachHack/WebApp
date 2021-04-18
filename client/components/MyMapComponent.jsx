fbc90aef530ef43dc654d11c30339f4c8cc2afde
Adnan Noori <noori.adnan@gmail.com>
2021-04-17 18:10:50 -0700

    - Updated map to render to user location

--- client/components/MyMapComponent.jsx
+++ client/components/MyMapComponent.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 
-const MyMapComponent = withScriptjs(withGoogleMap((props) =>
-  <GoogleMap
-    defaultZoom={8}
-    defaultCenter={{ lat: -34.397, lng: 150.644 }}
-  >
-    {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
-  </GoogleMap>
-))
+const MyMapComponent = withScriptjs(withGoogleMap((props) => {
+
+  return (
+    <GoogleMap
+      defaultZoom={8}
+      defaultCenter={{ lat: props.userLocation.latitude, lng: props.userLocation.longitude}}
+    >
+      {/* {props.isMarkerShown && <Marker position={{  lat: props.userLocation.latitude, lng: props.userLocation.longitude}} />} */}
+    </GoogleMap>
+  )
+}))
+
+
 
 export default MyMapComponent;
