15818459befc3670a6527d5be358ad19ed48275a
Adnan Noori <noori.adnan@gmail.com>
2021-04-17 18:11:12 -0700

    - Updated environment setup

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
    module: {
       rules: [
          {
-            test: /\.jsx?$/,
+            test: /\.(js|mjs|jsx|ts|tsx)$/,
             include: [
               path.join(__dirname, 'client'),
             ],
